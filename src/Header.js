1465019f3d12185ca10c22c14d9722d6755c106b
Bharath Kumar K <bkumarkb110@gmail.com>
2023-06-30 16:39:10 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Header = function() {
-    const headerStyle ={textAlign : 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase'};
+    const headerStyle ={textAlign : 'center', 
+    padding: 20, 
+    background: '#000', 
+    color: '#fff', 
+    textTransform: 'uppercase'};
     return(
         <div style={headerStyle}>
             Phone Directory
